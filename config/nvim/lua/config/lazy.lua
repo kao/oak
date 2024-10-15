394f513803714d51d2fa9bafc6cd05f73ab0a8c0
Flavien Cogez <flavien.cogez@gmail.com>
2024-10-15 14:51:58 +0200

    Add some extra plugins to neovim config

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -18,8 +18,19 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import/override with your plugins
     { import = "lazyvim.plugins.extras.editor.fzf" },
+    { import = "lazyvim.plugins.extras.editor.illuminate" },
+    { import = "lazyvim.plugins.extras.editor.aerial" },
+
+    { import = "lazyvim.plugins.extras.util.dot" },
+
+    { import = "lazyvim.plugins.extras.lang.ruby" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.git" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
+
+    -- import/override with your plugins
     { import = "plugins" },
   },
   defaults = {
@@ -31,7 +42,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  install = { colorscheme = { "catppuccin-mocha" } },
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
