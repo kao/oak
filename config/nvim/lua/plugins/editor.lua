b450901b0cc8a8d106a348fcf68d1c19011ae64b
Flavien Cogez <flavien.cogez@gmail.com>
2024-10-23 21:10:57 +0200

    nvim > lazygit, fzf, & more

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -13,4 +13,108 @@ return {
       }
     end,
   },
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      'MunifTanjim/nui.nvim',
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      'rcarriga/nvim-notify',
+    },
+    config = function()
+      require('noice').setup {
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+            ['vim.lsp.util.stylize_markdown'] = true,
+            ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+          },
+        },
+        -- you can enable a preset for easier configuration
+        presets = {
+          bottom_search = true, -- use a classic bottom cmdline for search
+          command_palette = true, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false, -- add a border to hover docs and signature help
+        },
+      }
+    end,
+  },
+
+  {
+    'ibhagwan/fzf-lua',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('fzf-lua').setup {}
+
+      local maps = vim.keymap
+      if vim.fn.executable 'git' == 1 then
+        maps.set('n', '<Leader>gb', function()
+          require('fzf-lua').git_branches()
+        end, { desc = 'Git branches' })
+        maps.set('n', '<Leader>gc', function()
+          require('fzf-lua').git_commits()
+        end, { desc = 'Git commits (repository)' })
+        maps.set('n', '<Leader>gC', function()
+          require('fzf-lua').git_bcommits()
+        end, { desc = 'Git commits (current file)' })
+        maps.set('n', '<Leader>gt', function()
+          require('fzf-lua').git_status()
+        end, { desc = 'Git status' })
+      end
+      maps.set('n', '<Leader>f<CR>', function()
+        require('fzf-lua').resume()
+      end, { desc = 'Resume previous search' })
+      maps.set('n', "<Leader>f'", function()
+        require('fzf-lua').marks()
+      end, { desc = 'Find marks' })
+      maps.set('n', '<Leader>f/', function()
+        require('fzf-lua').lgrep_curbuf()
+      end, { desc = 'Find words in current buffer' })
+      maps.set('n', '<Leader>fb', function()
+        require('fzf-lua').buffers()
+      end, { desc = 'Find buffers' })
+      maps.set('n', '<Leader>fc', function()
+        require('fzf-lua').grep_cword()
+      end, { desc = 'Find word under cursor' })
+      maps.set('n', '<Leader>fC', function()
+        require('fzf-lua').commands()
+      end, { desc = 'Find commands' })
+      maps.set('n', '<Leader>ff', function()
+        require('fzf-lua').files()
+      end, { desc = 'Find files' })
+      maps.set('n', '<Leader>fh', function()
+        require('fzf-lua').helptags()
+      end, { desc = 'Find help' })
+      maps.set('n', '<Leader>fk', function()
+        require('fzf-lua').keymaps()
+      end, { desc = 'Find keymaps' })
+      maps.set('n', '<Leader>fm', function()
+        require('fzf-lua').manpages()
+      end, { desc = 'Find man' })
+      maps.set('n', '<Leader>fo', function()
+        require('fzf-lua').oldfiles()
+      end, { desc = 'Find history' })
+      maps.set('n', '<Leader>fr', function()
+        require('fzf-lua').registers()
+      end, { desc = 'Find registers' })
+      maps.set('n', '<Leader>ft', function()
+        require('fzf-lua').colorschemes()
+      end, { desc = 'Find themes' })
+      if vim.fn.executable 'rg' == 1 or vim.fn.executable 'grep' == 1 then
+        maps.set('n', '<Leader>fw', function()
+          require('fzf-lua').live_grep_native()
+        end, { desc = 'Find words' })
+      end
+      -- maps.n["<Leader>ls"] = { function() require("fzf-lua").lsp_document_symbols() end, desc = "Search symbols" }
+    end,
+  },
 }
