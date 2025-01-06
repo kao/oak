7ddf3efb188a94af1a3b059469177a49dce8d122
Flavien Cogez <flavien.cogez@gmail.com>
2025-01-06 17:13:17 +0100

    neovim > add lazy extra language plugins

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -29,6 +29,9 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.git" },
     { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.toml" },
 
     -- import/override with your plugins
     { import = "plugins" },
