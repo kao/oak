5f92c8117c13c35163cafcb7af51ac80a63a0083
Flavien Cogez <flavien.cogez@gmail.com>
2025-03-19 22:35:44 +0100

    add keymaps

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -15,5 +15,18 @@ if not pcall(require, "lazy") then
   vim.cmd.quit()
 end
 
+-- TODO: move it elsewhere
+-- Beginning/End of line
+vim.keymap.set("n", "<C-e>", "<End>", { desc = "Move cursor to end of line in normal mode" })
+vim.keymap.set("i", "<C-e>", "<End>", { desc = "Move cursor to end of line in insert mode" })
+vim.keymap.set("v", "<C-e>", "<End>", { desc = "Move cursor to end of line in visual mode" })
+vim.keymap.set("n", "<C-a>", "<Home>", { desc = "Move cursor to beginning of line in normal mode" })
+vim.keymap.set("i", "<C-a>", "<Home>", { desc = "Move cursor to beginning of line in insert mode" })
+vim.keymap.set("v", "<C-a>", "<Home>", { desc = "Move cursor to beginning of line in visual mode" })
+
+-- Buffers
+vim.keymap.set("n", "<C-i>", "<cmd>bnext<cr>", { desc = "Next buffer" })
+vim.keymap.set("n", "<C-u>", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
+
 require "lazy_setup"
 require "polish"
